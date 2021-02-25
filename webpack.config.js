7e3b38efe04810f0fada9ccb081bebf6b7c7aeb1
aleksandr gulchenko <ogulchenko@contuctor.com>
2021-02-25 15:20:27 +0200

    asd

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,6 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 
 
 module.exports = federations({
-  name: 'condLayouts',
-  remotes: {
-    condRootMf: 'condRootMf@http://localhost:3001/remoteEntry.js',
-    condAuthPages: 'condAuthPages@http://localhost:3002/remoteEntry.js',
-    condFormElements: 'condFormElements@http://localhost:3004/remoteEntry.js',
-  },
   exposes: {
     './AuthLayout': './src/app/layouts/authLayout',
   },
